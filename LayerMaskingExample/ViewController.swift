d8d92bc60377b992a91c6c5af4d930f3b3e809e4
Soyoung Gwak <rhkrthdud7@gmail.com>
2020-03-08 16:36:40 +0900

    added masking logic

--- LayerMaskingExample/ViewController.swift
+++ LayerMaskingExample/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let bounds = imageView.bounds
+        let width = bounds.width
+        
+        let pathCircle = UIBezierPath(ovalIn: bounds)
+        let pathClip = UIBezierPath(rect: bounds).reversing()
+        pathClip.append(UIBezierPath(ovalIn: CGRect(x: width/4*0, y: width/4*0, width: width/4, height: width/4)))
+        pathClip.append(UIBezierPath(ovalIn: CGRect(x: width/4*1, y: width/4*1, width: width/4, height: width/4)))
+        pathClip.append(UIBezierPath(ovalIn: CGRect(x: width/4*2, y: width/4*2, width: width/4, height: width/4)))
+        pathClip.append(UIBezierPath(ovalIn: CGRect(x: width/4*3, y: width/4*3, width: width/4, height: width/4)))
+        pathClip.append(UIBezierPath(ovalIn: CGRect(x: width/4*0, y: width/4*3, width: width/4, height: width/4)))
+        pathClip.append(UIBezierPath(ovalIn: CGRect(x: width/4*1, y: width/4*2, width: width/4, height: width/4)))
+        pathClip.append(UIBezierPath(ovalIn: CGRect(x: width/4*2, y: width/4*1, width: width/4, height: width/4)))
+        pathClip.append(UIBezierPath(ovalIn: CGRect(x: width/4*3, y: width/4*0, width: width/4, height: width/4)))
+
+        let image = UIGraphicsImageRenderer(bounds: bounds).image { (c) in
+            UIColor.black.setFill()
+            pathClip.addClip()
+            pathCircle.fill()
+        }
+        let layer = CAShapeLayer()
+        layer.frame = bounds
+        layer.contents = image.cgImage
+        imageView.layer.mask = layer
     }
 
 
